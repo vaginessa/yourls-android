3f70aa8415f7acb57261335fb12c6f381d35ddc1
Mate <mate@bunteban.de>
2015-09-30 17:10:02 +0200

    Application class

---
+++ ayourlsapp/src/main/java/AyourlsApplication.java
@@ -0,0 +1,12 @@
+import android.app.Application;
+
+/**
+ * Created by mate on 30.09.2015.
+ */
+public class AyourlsApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
