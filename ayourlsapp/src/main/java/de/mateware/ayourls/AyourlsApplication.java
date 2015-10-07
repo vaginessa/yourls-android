68825464d7690c1c90065505c26ee29db073c2aa
Mate <mail@bunteban.de>
2015-10-07 23:18:26 +0200

    Crashlytics Integration

--- ayourlsapp/src/main/java/de/mateware/ayourls/AyourlsApplication.java
+++ ayourlsapp/src/main/java/de/mateware/ayourls/AyourlsApplication.java
@@ -2,8 +2,11 @@ package de.mateware.ayourls;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
+
 import de.mateware.ayourls.clipboard.ClipboardHelper;
-import de.mateware.ayourls.model.Link;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by mate on 30.09.2015.
@@ -12,7 +15,10 @@ public class AyourlsApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        CrashlyticsCore core = new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build();
+        Fabric.with(this, new Crashlytics.Builder().core(core)
+                                                   .build());
         ClipboardHelper.checkClipboardActivation(this);
-        new Link();
+        //new Link();
     }
 }
--- ayourlsapp/src/main/java/de/mateware/ayourls/model/Link.java
+++ ayourlsapp/src/main/java/de/mateware/ayourls/model/Link.java
@@ -72,5 +72,51 @@ public class Link {
         return cv;
     }
 
+    public String getKeyword() {
+        return keyword;
+    }
 
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public long getClicks() {
+        return clicks;
+    }
+
+    public void setClicks(long clicks) {
+        this.clicks = clicks;
+    }
 }
