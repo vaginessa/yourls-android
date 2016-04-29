ac08e55c4b5061d51f229c456f676c6146051ba6
Mate <ayourls@bunteban.de>
2016-04-29 15:12:30 +0200

    wizard experiment

---
+++ ayourlsapp/src/main/java/de/mateware/ayourls/wizard/YourlsWizardActivity.java
@@ -0,0 +1,16 @@
+package de.mateware.ayourls.wizard;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mate on 28.04.2016.
+ */
+public class YourlsWizardActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
